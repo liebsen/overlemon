150a24089c7ea46cc50f91c84ae3bc57f07a22ab
liebsen <overlemonsoft@gmail.com>
2021-01-23 18:45:15 -0300

    build

--- server.js
+++ server.js
@@ -44,9 +44,12 @@ app.use(cors({
 */
 
 var shuffle = a => {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    [a[i], a[j]] = [a[j], a[i]]
+  var j, x, i
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1))
+      x = a[i]
+      a[i] = a[j]
+      a[j] = x
   }
   return a
 }
